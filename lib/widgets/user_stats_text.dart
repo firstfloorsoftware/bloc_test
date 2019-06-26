99e0c0ad19e082fa41ba62dd3664d3660b09ea15
Koen Zwikstra <>
2019-06-26 21:39:37 +0200

    fixes null exception when UserStats not found

--- lib/widgets/user_stats_text.dart
+++ lib/widgets/user_stats_text.dart
@@ -8,16 +8,19 @@ class UserStatsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-        [
-          if (stats.count > 0)
-            '${stats.count} user${stats.count != 1 ? 's' : ''}'
-          else
-            'No users found',
-          if (stats.online > 0) '${stats.online} online',
-          if (stats.favorite > 0)
-            '${stats.favorite} favorite${stats.favorite != 1 ? 's' : ''}'
-        ].join(', '),
+    final text = stats == null || stats.count == 0
+        ? 'No users found'
+        : [
+            if (stats.count > 0)
+              '${stats.count} user${stats.count != 1 ? 's' : ''}'
+            else
+              'No users found',
+            if (stats.online > 0) '${stats.online} online',
+            if (stats.favorite > 0)
+              '${stats.favorite} favorite${stats.favorite != 1 ? 's' : ''}'
+          ].join(', ');
+
+    return Text(text,
         style: Theme.of(context).textTheme.body1.copyWith(color: Colors.grey));
   }
 }
